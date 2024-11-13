7a2f452f40e7d6191f45cad3713f563ddb2af432
jpbertti <joaobertty@gmail.com>
2024-11-13 12:02:29 -0300

    Adicionar arquivos de projeto.

---
+++ lacos_repeticao/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace lacos_repeticao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSair = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnMostar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lsbNumeros = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(36, 357);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(98, 40);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(197, 357);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(98, 40);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnMostar
+            // 
+            this.btnMostar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostar.Location = new System.Drawing.Point(353, 357);
+            this.btnMostar.Name = "btnMostar";
+            this.btnMostar.Size = new System.Drawing.Size(98, 40);
+            this.btnMostar.TabIndex = 2;
+            this.btnMostar.Text = "Mostrar";
+            this.btnMostar.UseVisualStyleBackColor = true;
+            this.btnMostar.Click += new System.EventHandler(this.btnMostar_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(144, 54);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(187, 24);
+            this.lblTitulo.TabIndex = 3;
+            this.lblTitulo.Text = "Números de 1 a 10";
+            // 
+            // lsbNumeros
+            // 
+            this.lsbNumeros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbNumeros.FormattingEnabled = true;
+            this.lsbNumeros.ItemHeight = 20;
+            this.lsbNumeros.Location = new System.Drawing.Point(36, 133);
+            this.lsbNumeros.Name = "lsbNumeros";
+            this.lsbNumeros.Size = new System.Drawing.Size(164, 164);
+            this.lsbNumeros.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 450);
+            this.Controls.Add(this.lsbNumeros);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnMostar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnSair);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnMostar;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.ListBox lsbNumeros;
+    }
+}
+
---
+++ lacos_repeticao/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lacos_repeticao
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente sair?", "Sair", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                Close();
+
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            lsbNumeros.Items.Clear();
+        }
+
+        private void btnMostar_Click(object sender, EventArgs e)
+        {
+            lsbNumeros.Items.Clear();
+            for (int i = 1; i <= 10; i++)
+            {
+                lsbNumeros.Items.Add(i);
+                
+
+            }
+           
+            
+        }
+    }
+}
